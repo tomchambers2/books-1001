1f8c685e38220d30e9cde9e915dbac8efd9c3482
Tom Chambers <tom.chambers@gmail.com>
2016-05-06 15:22:43 +0100

    recovery email

---
+++ routes/views/recover.js
@@ -0,0 +1,48 @@
+var keystone = require('keystone');
+var Book = keystone.list('Book');
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+exports = module.exports = function(req, res) {
+	
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+	
+	view.on('post', function(next) {
+		Book.model.findOne({
+			email: req.body.email
+		}).exec(function(err, result) {
+			locals.recovered = true;
+
+			if (!result) {
+				return next();
+			}
+
+			new keystone.Email({
+				templateExt: 'swig',
+	    		templateEngine: require('swig'),
+	    		templateName: 'recover'
+	    	}).send({
+				to: locals.book.email,
+				from: {
+					name: 'A Thousand and One Books',
+					email: 'librarian@athousandandonebooks.com'
+				},
+				subject: 'A reminder of your book number at A Thousand and One Books',
+				book: result,
+				number: pad(result.number, 4)
+			}, function(err, res) {
+				if (err) console.error(err);
+			});
+
+			return next(err);
+		});
+	})	
+
+	view.render('add-book');
+	
+};
