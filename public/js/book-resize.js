0528143a26686484f497060fc665493129b2590f
Tom Chambers <tom.chambers@gmail.com>
2016-05-24 13:17:14 +0100

    basic styling of homepage

---
+++ public/js/book-resize.js
@@ -0,0 +1,29 @@
+var min = 1;
+var max = 4;
+
+function updateSize(size) {
+	$('.plus').removeClass('disabled');
+	$('.minus').removeClass('disabled');
+	if (size == max) {
+		$('.plus').addClass('disabled');
+	} else if (size == min) {
+		$('.minus').addClass('disabled');
+	}
+	$('.books').attr('class', 'books').addClass('books-' + size);
+}
+
+$(document).on('ready', function() {
+	var size = 2;
+
+	$('.plus').on('click', function() {
+		size++;
+		size = Math.min(size, max);
+		updateSize(size);
+	});
+
+	$('.minus').on('click', function() {
+		size--;
+		size = Math.max(size, min);
+		updateSize(size);
+	});
+})
