2c29bee146a93de8183768e344fd73710c1c51e1
Tom Chambers <tom.chambers@gmail.com>
2016-06-04 09:50:46 +0100

    fix magnification issue

--- routes/views/index.js
+++ routes/views/index.js
@@ -2,16 +2,18 @@ var keystone = require('keystone');
 var Book = keystone.list('Book');
 
 exports = module.exports = function(req, res) {
-	
+
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	
+
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
 
 	view.on('init', function(next) {
-		Book.model.find().exec().then(function(result) {
+		Book.model.find({
+			title: { $exists: true }
+		}).exec().then(function(result) {
 			locals.books = result;
 			next();
 		}, function(err) {
@@ -20,6 +22,6 @@ exports = module.exports = function(req, res) {
 	});
 
 	console.log('will render')
-	
+
 	view.render('index');
 };
