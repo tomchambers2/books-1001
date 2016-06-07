b99b909e2d79abf7cfa5b0a8338410c4766e6af0
Tom Chambers <tom.chambers@gmail.com>
2016-06-07 10:11:05 +0100

    fix last row isseu

--- public/js/book-resize.js
+++ public/js/book-resize.js
@@ -4,35 +4,49 @@ var max = 4;
 function updateSize(size) {
 	$('.plus').removeClass('disabled');
 	$('.minus').removeClass('disabled');
-	if (size == max) {
+	if (size == min) {
 		$('.plus').addClass('disabled');
-	} else if (size == min) {
+	} else if (size == max) {
 		$('.minus').addClass('disabled');
 	}
 	$('.books').attr('class', 'books').addClass('books-' + size);
+
+	$('.books .book-void').remove();
+	fixLastRow(size);
+}
+
+function fixLastRow(size) {
+	var overspill = $('.books').children().length % size;
+	var requiredVoidBooks = size - overspill;
+	if ($('.books').children().length % size > 0) {
+		for (var i = 0; i < requiredVoidBooks; i++) {
+			$('.books').append('<div class="book-item book-void"></div>')
+		}
+	}
 }
 
 $(document).on('ready', function() {
 	var size = 2;
 
+	fixLastRow(size);
+
 	$(window).on('resize', function() {
 		if ($(this).width() < 768) {
-			console.log('small')
 			updateSize(4);
 		} else {
 			updateSize(2);
 		}
-	})
+	});
 
 	$('.plus').on('click', function() {
-		size++;
-		size = Math.min(size, max);
+		size--;
+		size = Math.max(size, min);
 		updateSize(size);
 	});
 
 	$('.minus').on('click', function() {
-		size--;
-		size = Math.max(size, min);
+		size++;
+		size = Math.min(size, max);
 		updateSize(size);
 	});
 })
