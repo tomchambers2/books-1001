65e661a351b2bf48b3b15b7e2b235f802da4abc9
Tom Chambers <tom.chambers@gmail.com>
2016-06-17 17:19:18 +0100

    add js files

---
+++ public/js/comment-dropdown.js
@@ -0,0 +1,5 @@
+$(document).on('ready', function() {
+	$('.add-comment-link').click(function() {
+		$('.add-comment').slideDown(1000);
+	})
+});
---
+++ public/js/show-search.js
@@ -0,0 +1,6 @@
+$(document).on('ready', function() {
+	$('.search-title').on('click', function() {
+		$('.search-title').hide();
+		$('.search-input').css('visibility', 'visible');
+	})
+});
